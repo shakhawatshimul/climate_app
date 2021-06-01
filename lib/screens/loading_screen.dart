bb79fa19523acd26f35cbec06a1e066d72ed9116
Md. Shakhawat Hossain <shakhawatshimul@gmail.com>
2021-06-01 12:50:38 +0600

    first commit

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,42 @@
+import 'package:climate_app/screens/location_screen.dart';
+import 'package:climate_app/services/weather.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:climate_app/screens/location_screen.dart';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    super.initState();
+    getLocationData();
+  }
+
+  void getLocationData() async {
+    var weatherData = await WeatherModel().getWeatherLocation();
+
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) {
+          return LocationScreen(locationWeather: weatherData);
+        },
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SpinKitDoubleBounce(
+          color: Colors.blueGrey,
+        ),
+      ),
+    );
+  }
+}
